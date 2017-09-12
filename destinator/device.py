85f4dbcb62d827d1595ed2f449ef4e6587b68eda
Peter Ullrich <peter.j.ullrich@gmail.com>
2017-09-12 15:17:03 +0200

    Added basic broadcast and receive functions

--- destinator/device.py
+++ destinator/device.py
@@ -1,17 +1,43 @@
+import socket
+import struct
+from threading import Thread
+
+
 class Device:
     """Base class for Nodes"""
 
-    def __init__(self):
+    socket = None
+
+    def __init__(self, category):
+        self.category = category
+
         self.connect()
 
+        self.listen()
+
     def connect(self):
-        pass
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind((self.category.MCAST_ADDR, self.category.MCAST_PORT))
 
-    def broadcast(self):
-        pass
+        membership = struct.pack("4sl",
+                                 socket.inet_aton(self.category.MCAST_ADDR),
+                                 socket.INADDR_ANY)
+        self.socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, membership)
 
-    def receive(self, msg):
-        pass
+    def listen(self):
+        t = Thread(target=self.receive)
+        t.start()
+
+    def broadcast(self, msg):
+        print("Sending message")
+        self.socket.sendto(msg.encode(), (self.category.MCAST_ADDR,
+                                          self.category.MCAST_PORT))
+
+    def receive(self):
+        while True:
+            message, address = self.socket.recv(255)
+            print(message)
 
     def deliver(self, msg):
         pass
